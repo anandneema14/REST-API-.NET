e76fbb4733bac51ad215523615d78b49a5596ac5
Anand Neema <anand.neema14@gmail.com>
2025-03-02 18:06:01 +0530

    Introduced Mapping

--- RESTAPIs/Movies.API/Controllers/MoviesController.cs
+++ RESTAPIs/Movies.API/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Movies.API.Mapping;
 using Movies.Application.Models;
 using Movies.Application.Repositories;
 using Movies.Contracts.Requests;
@@ -19,13 +20,7 @@ public class MoviesController : ControllerBase
     [HttpPost("movies")]
     public async Task<IActionResult> CreateMovieAsync(CreateMovieRequest request)
     {
-        var movie = new Movie
-        {
-            Id = Guid.NewGuid(),
-            Title = request.Title,
-            YearOfRelease = request.YearOfRelease,
-            Genres = request.Genres.ToList()
-        };
+        var movie = request.MapToMovie();//Moved the mapping logic to Contract Mapper
         await _movieRepository.CreateAsync(movie);
         //return Ok(movie);
         //Better to return Created and we should not return Ok
